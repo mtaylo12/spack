92597c83f67d542c0e93e00ce5b8ae0d3b7b6612
Maya Taylor <maya_taylor@brown.edu>
2022-06-23 13:49:36 -0700

    broken scream project file, end of workday

--- var/spack/repos/builtin/packages/scream/package.py
+++ var/spack/repos/builtin/packages/scream/package.py
@@ -27,19 +27,23 @@ class Scream(CMakePackage):
     """E3SM Scream Model"""
 
     homepage = "https://github.com/E3SM-Project/scream"
-    url      = "https://github.com/E3SM-Project/scream/archive/refs/tags/SCREAMv2.0.0-beta.3.tar.gz"
+#    url      = "https://github.com/E3SM-Project/scream/archive/refs/tags/SCREAMv2.0.0-beta.3.tar.gz"
+    url      = "https://github.com/mtaylo12/scream.git"
+
 
     # FIXME: Add a list of GitHub accounts to
     # notify when the package is updated.
     # maintainers = ['github_user1', 'github_user2']
 
-    
-    version('2.0.0-beta.3', git="https://github.com/E3SM-Project/scream.git", tag="2.0.0-beta.3")
+    version(git="https://github.com/mtaylo12/scream.git", commit="fcf28ecbac2c4d5aab7c727b4834f89433b20ae2",submodules=True)
+    #version('v2.0.0-beta.3', git="https://github.com/E3SM-Project/scream.git", tag="v2.0.0-beta.3")
     #sha256='71324f8d8290e7ef1143e2443e6e7b1a1ca8bcefb9168f418102e04053007780')
 
     # FIXME: Add dependencies if required.
     depends_on('cmake@3.23.1',type='build')
 
+    depends_on('python@3.8.9')
+    depends_on('gettext@0.21%intel@2021.6.0 cflags="-gcc-name=/usr/tce/packages/gcc/gcc-10.2.1/bin/gcc"')
     depends_on('m4@1.4.19%intel@2021.6.0 cflags="-gcc-name=/usr/tce/packages/gcc/gcc-10.2.1/bin/gcc"')
     depends_on('tar@1.34%intel@2021.6.0  cflags="-gcc-name=/usr/tce/packages/gcc/gcc-10.2.1/bin/gcc"')
     depends_on('mpich')
@@ -48,7 +52,7 @@ class Scream(CMakePackage):
     depends_on('cuda')
     depends_on('parallel-netcdf')
 
-    root_cmakelists_dir='components/scream'
+    root_cmakelists_dir='components'
     install_targets = ['baseline']
 
     conflicts('util-linux-uuid@2.36.3:', when='%intel')
@@ -58,7 +62,8 @@ class Scream(CMakePackage):
         # FIXME: Add arguments other than
         # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
         # FIXME: If not needed delete this function
-        args = ['-D CMAKE_BUILD_TYPE=Debug',
+        args = [
+            '-D CMAKE_BUILD_TYPE=Debug',
                 '-D Kokkos_ENABLE_DEBUG=TRUE',
                 '-D Kokkos_ENABLE_AGGRESSIVE_VECTORIZATION=OFF',
                 '-D Kokkos_ENABLE_SERIAL=ON',
@@ -69,6 +74,7 @@ class Scream(CMakePackage):
                 '-D CMAKE_C_COMPILER=mpicc',
                 '-D CMAKE_CXX_COMPILER=mpicxx',
                 '-D CMAKE_Fortran_COMPILER=mpif90',
-                '-C ${SCREAM_SRC_DIR}/components/scream/cmake/machine-files/${HOSTNAME}.cmake',
-                ]
-        return 
+                '-D SCREAM_INPUT_ROOT=/usr/gdata/climdat/ccsm3data/inputdata"'
+
+        ]
+        return args
