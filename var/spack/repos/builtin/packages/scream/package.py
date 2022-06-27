895d2c7d47fd5a126ae54a517b64061f79083016
Maya Taylor <maya_taylor@brown.edu>
2022-06-27 13:15:12 -0700

    scream package with pycache

--- var/spack/repos/builtin/packages/scream/package.py
+++ var/spack/repos/builtin/packages/scream/package.py
@@ -24,30 +24,22 @@ from spack.package import *
 
 
 class Scream(CMakePackage):
-    """E3SM Scream Model"""
+    """FIXME: Put a proper description of your package here."""
 
+# FIXME: Add a proper url for your package's homepage here.                                                                                                                    
     homepage = "https://github.com/E3SM-Project/scream"
-#    url      = "https://github.com/E3SM-Project/scream/archive/refs/tags/SCREAMv2.0.0-beta.3.tar.gz"
-    url      = "https://github.com/mtaylo12/scream.git"
+    url      = "https://github.com/E3SM-Project/scream/archive/refs/tags/scream-v1.0.0-alpha.0.1.tar.gz"
 
+    # FIXME: Add a list of GitHub accounts to                                                                                                                                      
+    # notify when the package is updated.                                                                                                                                          
+    # maintainers = ['github_user1', 'github_user2']                                                                                                                               
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
+    version('1.0.0-alpha.0.1',git="https://github.com/E3SM-Project/scream.git", tag="scream-v1.0.0-alpha.0.1",submodules=True)
+    #sha256='45e9293d36a7505283378f3d98206e593e963115b5c8624effda3859538b2515')                                                                                                    
 
-    #version(git="https://github.com/mtaylo12/scream.git", commit="fcf28ecbac2c4d5aab7c727b4834f89433b20ae2",submodules=True)
-    #version('v2.0.0-beta.3', git="https://github.com/E3SM-Project/scream.git", tag="v2.0.0-beta.3")
-    #sha256='71324f8d8290e7ef1143e2443e6e7b1a1ca8bcefb9168f418102e04053007780')
-    version('scream-v1.0.0-alpha.0.1', git="https://github.com/E3SM-Project/scream.git", tag="scream-v1.0.0-alpha.0.1")
-
-    # FIXME: Add dependencies if required.
+    # FIXME: Add dependencies if required.                                                                                                                                         
     depends_on('cmake@3.23.1',type='build')
-
-    depends_on('python@3.8.9')
-    depends_on('gettext@0.21%intel@2021.6.0 cflags="-gcc-name=/usr/tce/packages/gcc/gcc-10.2.1/bin/gcc"')
-    depends_on('m4@1.4.19%intel@2021.6.0 cflags="-gcc-name=/usr/tce/packages/gcc/gcc-10.2.1/bin/gcc"')
-    depends_on('tar@1.34%intel@2021.6.0  cflags="-gcc-name=/usr/tce/packages/gcc/gcc-10.2.1/bin/gcc"')
-    depends_on('mpich')
+    depends_on('openmpi@4.1.2')
     depends_on('netcdf-fortran')
     depends_on('netcdf-c')
     depends_on('cuda')
@@ -58,13 +50,13 @@ class Scream(CMakePackage):
 
     conflicts('util-linux-uuid@2.36.3:', when='%intel')
     conflicts('diffutils@3.8:',when='%intel')
-    
+
     def cmake_args(self):
         # FIXME: Add arguments other than
         # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
         # FIXME: If not needed delete this function
         args = [
-            '-D CMAKE_BUILD_TYPE=Debug',
+        '-D CMAKE_BUILD_TYPE=Debug',
                 '-D Kokkos_ENABLE_DEBUG=TRUE',
                 '-D Kokkos_ENABLE_AGGRESSIVE_VECTORIZATION=OFF',
                 '-D Kokkos_ENABLE_SERIAL=ON',
@@ -76,6 +68,5 @@ class Scream(CMakePackage):
                 '-D CMAKE_CXX_COMPILER=mpicxx',
                 '-D CMAKE_Fortran_COMPILER=mpif90',
                 '-D SCREAM_INPUT_ROOT=/usr/gdata/climdat/ccsm3data/inputdata"'
-
         ]
         return args
