527032da842190b280e55e003f646fb7f960efc9
Maya Taylor <maya_taylor@brown.edu>
2022-07-12 11:11:22 -0700

    gcc10.2.1 build working on quartz

--- var/spack/repos/builtin/packages/scream/package.py
+++ var/spack/repos/builtin/packages/scream/package.py
@@ -69,12 +69,15 @@ class Scream(CMakePackage):
                 '-D SCREAM_MPI_NP_FLAG=-n',
                 '-D SCREAM_INPUT_ROOT=/usr/gdata/climdat/ccsm3data/inputdata']
         
-        if self.spec.satisfies('%gcc'):
+        if self.spec.satisfies('%gcc@8.3.1'):
             args += ['-DCMAKE_EXE_LINKER_FLAGS=-L/usr/tce/packages/gcc/gcc-8.3.1/rh/lib/gcc/x86_64-redhat-linux/8/',
                      '-DCMAKE_CXX_FLAGS=-w']
-            if self.spec.satisfies('%gcc@10:'):
-                args += ['-DCMAKE_Fortran_FLAGS=-fallow-argument-mismatch']
-                
+
+        if self.spec.satisfies('%gcc@10.2.1'):
+            args += ['-DCMAKE_Fortran_FLAGS=-fallow-argument-mismatch',
+                     '-DCMAKE_EXE_LINKER_FLAGS=-L/usr/tce/packages/gcc/gcc-10.2.1/rh/lib/gcc/x86_64-redhat-linux/10/',
+                     '-DCMAKE_CXX_FLAGS=-w']
+            
         elif self.spec.satisfies('%intel'):
             args += ['-DCMAKE_CXX_FLAGS=-w -cxxlib=/usr/tce/packages/gcc/gcc-8.3.1/rh',
                      '-DCMAKE_EXE_LINKER_FLAGS=-L/usr/tce/packages/gcc/gcc-8.3.1/rh/lib/gcc/x86_64-redhat-linux/8/ -mkl']
