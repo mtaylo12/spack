3f34509dd292e4db1104ff2e31d88fa1f298f4ef
Maya Taylor <maya_taylor@brown.edu>
2022-08-26 09:28:26 -0700

    editing comments in package

--- var/spack/repos/builtin/packages/e3sm-scream/package.py
+++ var/spack/repos/builtin/packages/e3sm-scream/package.py
@@ -3,33 +3,11 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install e3sm-scream
-#
-# You can edit this file again by typing:
-#
-#     spack edit e3sm-scream
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
 
 
 class E3smScream(Package):
-    """FIXME: Put a proper description of your package here."""
-    # INSTRUCTIONS (quartz)                                                                                                                                                                         
-    # set the directory that you want spack to stage the code in by 'export TMPDIR=...'                                                                                                               
-    # make sure both the code base and the spack repo are in directories shared across nodes. otherwise batch job will fail                                                                          
-    # run 'spack install --keep-stage e3sm-test%intel@19.0.4.227' or run with gcc@8.3.1'                                                                                                              
-    # everything should be loaded correctly, but 'spack load' any python packages needed if you get module missing errors                                                                           
+    """Package for E3SMs Scream dependencies. Doesn't install anything other than dependencies."""
 
     homepage = "https://e3sm.org/"
     url      = "https://github.com/E3SM-Project/scream"
